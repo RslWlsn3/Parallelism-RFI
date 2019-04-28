017cd1544f45d7056ae999d621e86c502662ebe4
Connor Mote <connor27mote@gmail.com>
2019-04-27 20:49:57 -0600

    Merge sort now works with strings

--- Parallelism-RFI/Parallelism-RFI/Source.cpp
+++ Parallelism-RFI/Parallelism-RFI/Source.cpp
@@ -16,8 +16,7 @@ int* create_random_nums(int size)
 	{
 		int x = rand() % 10000;  
 		randNums[i] = x;
-	}
-	
+	}	
 	return randNums;
 }
 
@@ -27,7 +26,59 @@ void PrintArray(int *array, int n) {
 	cout << endl;
 }
 
-void serialMerger(int arr[], int lo, int  mi, int hi) {
+void serialMerge(string currentArray[], int low, int mid, int high, int size)
+{
+	int i = low, j = mid + 1, k = low;
+	string *Temp;
+	Temp = new string[size];
+
+	while (i <= mid && j <= high)
+	{
+		if (currentArray[i] < currentArray[j])
+		{
+			Temp[k].assign(currentArray[i]);
+			i++;
+		}
+		else
+		{
+			Temp[k].assign(currentArray[j]);
+			j++;
+		}
+		k++;
+	}
+	if (i > mid)
+	{
+		for (int h = j; h <= high; h++)
+		{
+			Temp[k].assign(currentArray[h]);
+			k++;
+		}
+	}
+	else
+		for (int h = i; h <= mid; h++)
+		{
+			Temp[k].assign(currentArray[h]);
+			k++;
+		}
+	for (int i = low; i <= high; i++)
+	{
+		currentArray[i].assign(Temp[i]);
+	}
+}
+
+void serialMergeSort(string currentArray[], int low, int high, int size)
+{
+	int mid = 0;
+	if (low < high)
+	{
+		mid = ((low + high) / 2);
+		serialMergeSort(currentArray, low, mid, size);
+		serialMergeSort(currentArray, mid + 1, high, size);
+		serialMerge(currentArray, low, mid, high, size);
+	}
+}
+
+void serialMergeint(int arr[], int lo, int  mi, int hi) {
 	int *temp = new int[hi - lo + 1];//temporary merger array
 	int i = lo, j = mi + 1;//i is for left-hand,j is for right-hand
 	int k = 0;//k is for the temporary array
@@ -49,25 +100,35 @@ void serialMerger(int arr[], int lo, int  mi, int hi) {
 
 	delete[]temp;
 }
-void serialMergeSortHelper(int arr[], int lo, int hi) {
-	int mid;
-	if (lo < hi) {
-		mid = (lo + hi) >> 1;
-		serialMergeSortHelper(arr, lo, mid);
-		serialMergeSortHelper(arr, mid + 1, hi);
-		serialMerger(arr, lo, mid, hi);
+void serialMergeSortint(int currentArray[], int low, int high, int size)
+{
+	int mid = 0;
+	if (low < high)
+	{
+		mid = ((low + high) / 2);
+		serialMergeSortint(currentArray, low, mid, size);
+		serialMergeSortint(currentArray, mid + 1, high, size);
+		serialMergeint(currentArray, low, mid, high);
 	}
 }
-void serialMergeSort(int arr[], int arr_size) {
-	serialMergeSortHelper(arr, 0, arr_size - 1);
-}
-
-
 
 int main()
 {
+	//code to test merge sort with ints
 	int size = 50;
 	int* arr = create_random_nums(size);
-	serialMergeSort(arr, size);
-	PrintArray(arr, size);
+
+	serialMergeSortint(arr, 0, size - 1, size);
+	
+	//code to test merge sort with strings
+	/*int size= 3;	
+	string *arr;
+	arr = new string[size]{ "John", "Connor", "bill" };	
+	
+
+	serialMergeSort(arr, 0, size - 1, size);*/
+	
+	//print out results
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << endl;
 }
